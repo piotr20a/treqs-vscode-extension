f35381ac2b9105eff28d64e0f51ea5c5db61b7ac
Piotr Abramek <piotr20@ru.is>
2021-06-23 13:34:52 +0000

    add webview

--- out/extension.js
+++ out/extension.js
@@ -3,21 +3,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.deactivate = exports.activate = void 0;
 const vscode = require("vscode");
 const cp = require("child_process");
+let { PythonShell } = require('python-shell');
+var packageName = 'treqs';
+let options = {
+    args: [packageName]
+};
 function activate(context) {
     console.log('Congratulations, your extension "treqs" is now active!');
     context.subscriptions.push(vscode.commands.registerCommand('treqs.helloWorld', () => {
-        vscode.window.showInformationMessage('Hello World from treqs!');
+        vscode.window.showInformationMessage('Hello World from treqs!', 'delete', 'bye');
     }));
-    context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', () => {
-        vscode.window.showInformationMessage('Hello World from treqs!');
-    }));
-    context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', () => {
-        cp.exec('pwd', (err, stdout, stderr) => {
-            console.log('stdout: ' + stdout);
-            console.log('stderr: ' + stderr);
-            if (err) {
-                console.log('error: ' + err);
-            }
+    context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', (res) => {
+        console.log(res.path);
+        PythonShell.run('..\\..\\..\\Desktop\\treqs-ng\\setup.py', options, function (err, result) {
+            if (err)
+                throw err;
+            else
+                console.log("it loaded treqs" + result);
+            cp.exec('treqs list' + res.path, (err, stdout, stderr) => {
+                console.log(err);
+                console.log(stdout);
+                console.log(stderr);
+            });
         });
     }));
     // context.subscriptions.push(vscode.commands.registerCommand('treqs.addOption', () => this.addOption()));
--- src/extension.ts
+++ src/extension.ts
@@ -1,30 +1,38 @@
 
 import * as vscode from 'vscode';
 import cp = require('child_process');
-
+import { HelloWorldPanel } from './webview';
+import { stderr, stdout } from 'process';
+let {PythonShell} = require('python-shell');
+var packageName = 'treqs';
+let options = {
+	args: [packageName]
+}
 export function activate(context: vscode.ExtensionContext) {
 
 	console.log('Congratulations, your extension "treqs" is now active!');
 
 	context.subscriptions.push(vscode.commands.registerCommand('treqs.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from treqs!');
+		vscode.window.showInformationMessage('Hello World from treqs!', 'delete', 'bye');
+
 
 	}));
-	context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', () => {
-		vscode.window.showInformationMessage('Hello World from treqs!');
-
-	}));
-
-	context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', () => {
-		cp.exec('pwd', (err, stdout, stderr) => {
-			console.log('stdout: ' + stdout);
-			console.log('stderr: ' + stderr);
-			if (err) {
-				console.log('error: ' + err);
-
-			}
-		});
-	}));
+	
+	context.subscriptions.push(vscode.commands.registerCommand('treqs.testCommand', (res) => {
+		console.log(res.path);
+		PythonShell.run('..\\..\\..\\Desktop\\treqs-ng\\setup.py', options,
+		function(err: any, result: string){
+		   if(err) throw err;
+		   else 
+		   console.log("it loaded treqs" + result);
+		   cp.exec('treqs list' + res.path,(err, stdout, stderr) =>{
+			   console.log(err);
+			   console.log(stdout);
+			   console.log(stderr);
+		   });
+		})
+	   })
+	);
 
 	// context.subscriptions.push(vscode.commands.registerCommand('treqs.addOption', () => this.addOption()));
 
